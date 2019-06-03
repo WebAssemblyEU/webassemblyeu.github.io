0731deb1e5465bd3dacc7ec07c0a4a650d1fa20a
Thomas Tränkler <github.com@thomas.traenkler.com>
2019-06-03 15:19:09 +0200

    Deploy Next.js to gh-pages

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "df6da544a19ec302a732"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "bb2a273278fa3228dd3f"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
