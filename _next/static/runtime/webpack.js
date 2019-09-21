89247c3abf59584396998a16f3503ce9f8895fec
Thomas Tränkler <github.com@thomas.traenkler.com>
2019-09-22 01:39:18 +0200

    bugfix for styles

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f279ec5c9ad2a8ee371e";
+/******/ 	var hotCurrentHash = "729fa4661b846e6f2380";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
